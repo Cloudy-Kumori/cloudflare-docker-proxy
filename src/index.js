b3f27ae30bd570889674cd932e906f714d078d75
Cloudy <47238899+Cloudy-Kumori@users.noreply.github.com>
2024-06-20 00:06:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.cloudy233.win": dockerHub,
+  "quay.cloudy233.win": "https://quay.io",
+  "gcr.cloudy233.win": "https://gcr.io",
+  "k8s-gcr.cloudy233.win": "https://k8s.gcr.io",
+  "k8s.cloudy233.win": "https://registry.k8s.io",
+  "ghcr.cloudy233.win": "https://ghcr.io",
+  "cloudsmith.cloudy233.win": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
